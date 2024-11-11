8fe0edce91a4796e23b575eb52bb158025f135ab
Mir Injamamul <injamam@bengalmobileqa.com>
2024-11-11 17:41:13 +0600

    port modified

--- src/App.jsx
+++ src/App.jsx
@@ -95,8 +95,8 @@ function App(props) {
     let url =
       window.location.protocol +
       "//" +
-      "server.geniusclass.co.uk"+
-      ":" + "8443";
+      "live.geniusclass.co.uk"+
+      ":" + "5551";
       // Note if you're running this inside docker you'll need to remove the ":5551" and possibly add the following line so that caddy can proxy correctly
       // + window.location.port;
     console.log("Connect changed url:" + url);
